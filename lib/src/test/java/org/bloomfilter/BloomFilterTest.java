2a512ac0aebf229e527290c58cb112b5b6e2c94e
Srivatsa <vatsa@macmini.home>
2025-09-06 18:12:55 +0100

    Added count and remove

--- lib/src/test/java/org/bloomfilter/BloomFilterTest.java
+++ lib/src/test/java/org/bloomfilter/BloomFilterTest.java
@@ -22,14 +22,55 @@ class BloomFilterTest {
 
     @Test
     void testReturnsFalseIfFilterValueGreaterThanSizeOfFilterArray() {
-        BloomFilter<ExampleBloomFilterValue> bloomFilter = new BloomFilter<>(10);
+        BloomFilter<ExampleBloomFilterValue> bloomFilter = new BloomFilter<>(1);
         assertFalse(bloomFilter.isAbsent(new ExampleBloomFilterValue(20)));
     }
 
     @Test
     void throwRuntimeExceptionIfFilterValueIsGreaterThanSizeOfFilterArray()
     {
-        BloomFilter<ExampleBloomFilterValue> bloomFilter = new BloomFilter<>(10);
+        BloomFilter<ExampleBloomFilterValue> bloomFilter = new BloomFilter<>(1);
         assertThrows(RuntimeException.class, () -> bloomFilter.add(new ExampleBloomFilterValue(20)));
+    }
+
+    @Test
+    void testCountValuesReturnsCorrectCount()
+    {
+        BloomFilter<ExampleBloomFilterValue> bloomFilter = new BloomFilter<>(5);
+        bloomFilter.add(new ExampleBloomFilterValue(1));
+        bloomFilter.add(new ExampleBloomFilterValue(2));
+        bloomFilter.add(new ExampleBloomFilterValue(3));
+        bloomFilter.add(new ExampleBloomFilterValue(4));
+
+        assertEquals(4,  bloomFilter.valuesCount());
+    }
+
+    @Test
+    void testRemoveReturnsTrueForIsAbsentCheck()
+    {
+        BloomFilter<ExampleBloomFilterValue> bloomFilter = new BloomFilter<>(5);
+        ExampleBloomFilterValue bloomFilterValue = new ExampleBloomFilterValue(0);
+        bloomFilter.add(bloomFilterValue);
+        assertFalse(bloomFilter.isAbsent(bloomFilterValue));
+        bloomFilter.remove(bloomFilterValue.getFilterValue());
+        assertTrue(bloomFilter.isAbsent(bloomFilterValue));
+    }
+
+    @Test
+    void testRemovingDifferentValueReturnsFalseIsAbsentCheck()
+    {
+        BloomFilter<ExampleBloomFilterValue> bloomFilter = new BloomFilter<>(5);
+        ExampleBloomFilterValue bloomFilterValue = new ExampleBloomFilterValue(0);
+        bloomFilter.add(bloomFilterValue);
+        assertFalse(bloomFilter.isAbsent(bloomFilterValue));
+        bloomFilter.remove(1);
+        assertFalse(bloomFilter.isAbsent(bloomFilterValue));
+    }
+
+    @Test
+    void throwRuntimeExceptionForRemoveIfFilterValueIsGreaterThanSizeOfFilterArray()
+    {
+        BloomFilter<ExampleBloomFilterValue> bloomFilter = new BloomFilter<>(1);
+        assertThrows(RuntimeException.class, () -> bloomFilter.remove(20));
     }
 }
